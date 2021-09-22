b3bb5093c4aa4036fb28b48635e8566bdd6b962f
Your Name <you@example.com>
2021-09-22 12:51:31 +0200

    basic routing config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from './Login';
 import './styles/styles.scss';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { AuthProvider } from './Auth';
+import  PrivateRoute  from './PrivateRoute';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // <React.StrictMode>
+  <AuthProvider>
+  <Router>
+    <div>
+      <PrivateRoute path="/" component={App} exact='true'/>
+      <Route path="login" component={Login} />
+    </div>
+  </Router>
+  </AuthProvider>
+  // </React.StrictMode>
+  ,document.getElementById('root')
 );
 
 reportWebVitals();
